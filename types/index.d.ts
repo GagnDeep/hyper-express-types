0142981e5eaae3bdfab18189e5ea1466911f0653
Gagandeep Singh <dp.gagn@gmail.com>
2021-10-21 13:38:28 +0000

    changes

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,13 +2,5 @@ import Server = require("./src/components/Server.js");
 import Router = require("./src/components/router/Router.js");
 
 
-
-
-declare module 'hyper-express'{
-	export const Server: Server	
-    export const Router: Router
-}
-
-
 export { Server, Router};
 
