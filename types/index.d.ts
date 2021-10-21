9c8f73cb0c7dbb16f34f99ca3a67df3a560cc649
Gagandeep Singh <dp.gagn@gmail.com>
2021-10-21 13:24:33 +0000

    uwebsocket error

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,15 @@
 import Server = require("./src/components/Server.js");
 import Router = require("./src/components/router/Router.js");
 
+
+
+
 declare module 'hyper-express'{
 	export const Server: Server	
     export const Router: Router
     export const compressors
 }
 
-export { Server, Router, uWebsockets as compressors };
+
+export { Server, Router};
 
